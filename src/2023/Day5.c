81a25c5e159186f04c9da0b43e11357ad8827c46
JKachele <justin@kachele.com>
2024-04-11 16:58:14 -0400

    Started Day5 Part2

--- src/2023/Day5.c
+++ src/2023/Day5.c
@@ -11,6 +11,7 @@
 #include "../util/linkedlist.h"
 #include "../util/inputFile.h"
 
+// Support Functions {{{
 void printLong(void* data) {
     if (data == NULL) {return;}
     long *num = (long*)data;
@@ -66,7 +67,7 @@ llNode *processSection(llNode *start, long in[], long out[], int numSeeds) {
     printLongArray(out, numSeeds);
 
     return current;
-}
+}   // }}}
 
 // Part 1 {{{
 void part1(llist *ll) {
@@ -139,15 +140,74 @@ void part1(llist *ll) {
 // Part 2 {{{
 void part2(llist *ll) {
     llNode *current = ll->head;
-    while(current != NULL) {
-        current = current->next;
+
+    char seedStr[BUFFER_SIZE];
+    strncpy(seedStr, (char*)current->data, BUFFER_SIZE);
+    llist *seedLL = llist_create();
+    char *token = strtok(seedStr, " ");
+    token = strtok(NULL, " ");
+    while (token != NULL) {
+        long *seedPtr = malloc(sizeof(long));
+        if (!seedPtr) {exit(-1);}
+        *seedPtr = strtol(token, NULL, 10);
+        llist_add(seedLL, seedPtr);
+        token = strtok(NULL, " ");
     }
-    printf("Part 2: \n");
+    long seeds[seedLL->length];
+    llToArray(seedLL, seeds);
+    int numSeeds = seedLL->length;
+    printLongArray(seeds, numSeeds);
+
+    // Skip next 2 lines to get to the seed to soil nums
+    current = current->next->next->next;
+
+    long soils[numSeeds];
+    initLongArray(soils, numSeeds, -1);
+    current = processSection(current, seeds, soils, numSeeds);
+    current = current->next->next;
+
+    long fert[numSeeds];
+    initLongArray(fert, numSeeds, -1);
+    current = processSection(current, soils, fert, numSeeds);
+    current = current->next->next;
+
+    long water[numSeeds];
+    initLongArray(water, numSeeds, -1);
+    current = processSection(current, fert, water, numSeeds);
+    current = current->next->next;
+
+    long light[numSeeds];
+    initLongArray(light, numSeeds, -1);
+    current = processSection(current, water, light, numSeeds);
+    current = current->next->next;
+
+    long temp[numSeeds];
+    initLongArray(temp, numSeeds, -1);
+    current = processSection(current, light, temp, numSeeds);
+    current = current->next->next;
+
+    long humidity[numSeeds];
+    initLongArray(humidity, numSeeds, -1);
+    current = processSection(current, temp, humidity, numSeeds);
+    current = current->next->next;
+
+    long location[numSeeds];
+    initLongArray(location, numSeeds, -1);
+    current = processSection(current, humidity, location, numSeeds);
+
+    long minLocation = location[0];
+    for (int i = 1; i < numSeeds; i++) {
+        if (location[i] < minLocation) {
+            minLocation = location[i];
+        }
+    }
+
+    printf("Part 2: Lowest Location = %ld\n", minLocation);
 }   // }}}
 
 int main(int argc, char *argv[]) {
-    llist *ll = getInputFile("assets/2023/Day5.txt");
-    // llist *ll = getInputFile("assets/test.txt");
+    // llist *ll = getInputFile("assets/2023/Day5.txt");
+    llist *ll = getInputFile("assets/test.txt");
     // llist_print(ll, printInput);
 
     part1(ll);
